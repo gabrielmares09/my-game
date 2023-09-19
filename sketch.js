27335d14321b261ea4239a996d95e74d0f7de54e
Bia <82560900+BiatrizR@users.noreply.github.com>
2023-09-19 19:03:05 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,21 +11,26 @@ function setup() {
   var canvas = createCanvas(400, 400);
   engine = Engine.create();
   world = engine.world;
-  var ball_options = {
-    restitution: 0.75
-  }
-  var chao_options = {
+ var chao_options = {
     isStatic: true
   }
-  chao = Bodies.rectangle(150, 380, 400, 400, 20, chao_options)
-  World.add(world, chao)
-  ball = Bodies.circle(100, 100, 20, ball_options)
+
+  var ball_options = {
+
+    restitution: 0.75
+  }
+
+  ball = Bodies.circle(100, 100, 40, ball_options)
   World.add(world, ball)
+
+
+  chao = Bodies.rectangle(200,580,400,20, chao_options);
+  World.add(world, chao);
 }
 
 function draw() {
   background(0);
   Engine.update(engine);
-  rect(chao.position.x, chao.position.y, 400, 20);
+  rect(chao.position.x, chao.position.y, 550, 40);
   ellipse(ball.position.x,ball.position.y,20);
 }
